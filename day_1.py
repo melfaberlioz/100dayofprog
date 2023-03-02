60f940829cacd23fbd943be82fa2d1328bdff25b
melfaberlioz <sofi.horizon@gmail.com>
2023-03-02 21:09:55 +0200

    Initial Commit

--- day_1.py
+++ day_1.py
@@ -3,3 +3,4 @@ city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print(f"Your band name could be {city + ' ' + pet}")
 
+# print("Your band name could be" + city + " " + pet)
---
+++ day_2.py
@@ -0,0 +1,29 @@
+# Data Types
+
+# String
+print('Hello'[0])   #it'll print 'H'.
+print('Hello'[-1])  #but here it'll print an 'o'.
+print('Hello'[-2])  #and here an 'l'
+
+# Number
+print(123_453_567)
+
+
+# num_char = len(input("What is your name?"))
+# # converting num_char into a string
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+# # to know which type of variable do we have
+# print(type(num_char))
+
+a = str(123)
+print(type(a))
+
+print(70 + float('100.5'))
+
+
+
+two_digit_num = input("Type a two digit numbers: ")
+first_digit = two_digit_num[0]
+second_digit = two_digit_num[1]
+print(int(first_digit) + int(second_digit))
