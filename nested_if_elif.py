922de23a83c355a69a9094e8b290ee844a577d54
melfaberlioz <sofi.horizon@gmail.com>
2023-03-21 20:46:00 +0200

    Initial Commit

---
+++ nested_if_elif.py
@@ -0,0 +1,13 @@
+height = int(input("Enter your height:"))
+
+if height >= 120:
+    print("You can ride!")
+    age = int(input("Enter your age:"))
+    if age < 12:
+        print("Please pay $5.")
+    elif age > 12 and age < 18:
+        print("Please pay $7.")
+    else:
+        print('Please pay $12')
+else:
+    print('Sorry you can\'t ride')
