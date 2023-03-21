6765d02a02da52b3f5e18dc239d67c9902afa67f
melfaberlioz <sofi.horizon@gmail.com>
2023-03-21 20:54:30 +0200

    Initial Commit

--- BMI2_0.py
+++ BMI2_0.py
@@ -5,11 +5,11 @@ BMI = round(weight/height**2)
 
 if BMI < 18.5:
     print(f'Your BMI is {BMI}. You are underweight.')
-elif BMI > 18.5 and BMI < 25:
+elif BMI < 25:
     print(f'Your BMI is {BMI}. You have a normal weight.')
-elif BMI > 25 and BMI < 30:
+elif BMI < 30:
     print(f'Your BMI is {BMI}. You are slightly overweight.')
-elif BMI > 30 and BMI < 35:
+elif BMI < 35:
     print(f'Your BMI is {BMI}. You are obese.')
 else:
     print(f'Your BMI is {BMI}. You are clinically obese.')
